1f0a943712fbd39c80d99bcb2d5c8c9c2859255b
Jim Knudstrup <jim.knudstrup@gmail.com>
2015-08-01 23:49:39 -0700

    Add server, update readme.

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # Focus EEG Classifier
-Currently rebuilding things, thank you for your patience.
+
+This is a classifier for attentive states that uses the Muse portable EEG headset. Given an experimental recording of brain wave data, it will attempt to classify attentive and non-attentive states, and will print live predictions to a terminal.
+
+If you follow the instructions outlined in this document, you can train your own classifier, potentially with far greater accuracy than off-the-shelf algorithms. Some of this process will seem a bit more... involved than I'd like it to be. Interaxon is (purportedly) working on a port for LibMuse to desktop OS', so hopefully I'll be able to correct that soon.
+
+##Dependencies
+
+* *Muse Headset:* This headset is required to interact with the Focus EEG Classifier. In principle, any kind of EEG would work with my methods, but you'd need to coerce the recordings into the right format.
+
+* *Muse SDK:* This will have all the lovely stuff you'll want to run the Muse on OSX. Speaking of which:
+
+* *OSX:* If you fix the paths and get all the dependencies right, you might be able to get this going on Windows or Linux, I just wouldn't know how.
---
+++ classifier_osc_server.py
@@ -0,0 +1,81 @@
+from liblo import *
+import sys
+import time
+import pickle
+import numpy as np
+import pandas as pd
+
+
+class MuseServer(ServerThread):
+    #listen for messages on port 5001
+    def __init__(self):
+        self.current_row = False
+        ServerThread.__init__(self, 5001)
+
+
+    @make_method('/muse/elements/alpha_absolute', 'ffff')
+    def alpha_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        self.current_row = [l_ear, l_forehead, r_forehead, r_ear]
+        pass
+
+
+    @make_method('/muse/elements/beta_absolute', 'ffff')
+    def beta_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        x = [l_ear, l_forehead, r_forehead, r_ear]
+        if len(self.current_row) == 4:
+            self.current_row += x
+        pass
+
+
+    @make_method('/muse/elements/delta_absolute', 'ffff')
+    def delta_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        x = [l_ear, l_forehead, r_forehead, r_ear]
+        if len(self.current_row) == 8:
+            self.current_row += x
+        pass
+
+
+    @make_method('/muse/elements/theta_absolute', 'ffff')
+    def theta_callback(self, path, args):
+        l_ear, l_forehead, r_forehead, r_ear = args
+        x = [l_ear, l_forehead, r_forehead, r_ear]
+        if len(self.current_row) == 12:
+            self.current_row += x
+        assert len(self.current_row) == 16
+
+        row = pd.Series(self.current_row)
+        condition_names = {1 : "Focused.", 2 : "Zoning Out!", 3 : "Wavering..."}
+        prediction = clf.predict(self.current_row)[0]
+        score = clf.decision_function(self.current_row)[0][0]
+        if (score > -3) and (score < 0): 
+            prediction = 3
+        name = condition_names[prediction], str(score)
+
+        if name[0] != "Focused.":
+            print name
+        else:
+            print(" ")
+            print(" ")
+        self.current_row = False
+        pass
+
+    #Handle unexpected messages
+    @make_method(None, None)
+    def fallback(self, path, args, types, src):
+        pass
+
+try:
+    server = MuseServer()
+except ServerError, err:
+    print str(err)
+    sys.exit()
+
+server.start()
+
+if __name__ == "__main__":
+    clf = pickle.load( open("med1.pkl", "rb" ) ) #choose appropriate filename
+    while 1:
+        time.sleep(1)
